677f3509c3be238598a2713264de25d57aff425a
dssysolyatin <sysolyatin.dima@yandex.ru>
2019-06-22 17:36:43 +0300

    Add encrypt, decrypt commands

--- cmd/ssecret/command/crypt/register.go
+++ cmd/ssecret/command/crypt/register.go
@@ -3,10 +3,6 @@ package crypt
 import "github.com/spf13/cobra"
 
 func Register(rootCmd *cobra.Command) {
-	generateCmd := &cobra.Command{
-		Use: "crypt",
-	}
 
-	rootCmd.AddCommand(generateCmd)
-	generateCmd.AddCommand(createEncryptCommand())
+	rootCmd.AddCommand(createCryptCommand())
 }
